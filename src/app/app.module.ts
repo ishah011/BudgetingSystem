79cdaf6aeebc8a6ef2cda580e691ba7c9d79cab7
Isheeta Shah <isheeta_s@yahoo.com>
2017-12-29 23:22:46 -0800

    created form component for adding expenses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ExpensesComponent } from './expenses/expenses.component';
+import { ExpenseFormComponent } from './expense-form/expense-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpensesComponent
+    ExpensesComponent,
+    ExpenseFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/expense-form/expense-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Expense} from '../expense';
+
+@Component({
+  selector: 'app-expense-form',
+  templateUrl: './expense-form.component.html',
+  styleUrls: ['./expense-form.component.css']
+})
+export class ExpenseFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
