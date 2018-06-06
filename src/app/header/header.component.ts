7995cbd75ec6517dfc1132d85f0850a513bcf580
Isheeta Shah <isheeta_s@yahoo.com>
2018-06-05 21:10:06 -0700

    start of navbar service functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from '../services/navbar.service'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers:[NavbarService]
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  selectedValue: string = "Home";
+  constructor(private _navbarService: NavbarService) { }
 
   ngOnInit() {
+    this._navbarService.setNavigation(this.selectedValue);
+  }
+
+  changeSelection(selection): void{
+    this.selectedValue = selection.srcElement.text;
+    this._navbarService.setNavigation(this.selectedValue);
   }
 
 }
