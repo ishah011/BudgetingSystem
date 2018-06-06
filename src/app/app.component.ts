dd21ee45e2b9a5fb5f814e99fa3e59d97b692996
Isheeta Shah <isheeta_s@yahoo.com>
2018-06-05 21:11:43 -0700

    start of navbar service functionality in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NavbarService } from './services/navbar.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NavbarService]
 })
 export class AppComponent {
   title = 'Budgeting System';
+  selectedValue:string;
+
+  constructor(private _navbarService: NavbarService){}
+
+  ngOnInit(){
+    this.selectedValue = this._navbarService.getNavigation();
+  }
 }
