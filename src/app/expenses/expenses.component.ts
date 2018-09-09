eccec5099744d848b28a2bd9302c6c2f5de61875
Isheeta Shah <isheeta_s@yahoo.com>
2018-09-08 23:29:16 -0700

    added styling changes to expenses page. added overview section to expenses with temp values in dropdown and monthly total

--- src/app/expenses/expenses.component.ts
+++ src/app/expenses/expenses.component.ts
@@ -13,6 +13,23 @@ export class ExpensesComponent implements OnInit {
   @Input() addedExpense: Expense;
   formVisible = false;
   title = 'Expenses Log';
+  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  tempTotals = [
+    {month: 'January', total: '1452.00'},
+    {month: 'February', total: '1561.02'},
+    {month: 'March', total: '1001.02'},
+    {month: 'April', total: '1578.08'},
+    {month: 'May', total: '1354.25'},
+    {month: 'June ', total: '1785.23'},
+    {month: 'July', total: '1002.54'},
+    {month: 'August', total: '985.24'},
+    {month: 'September', total: '1677.95'},
+    {month: 'October', total: '1652.48'},
+    {month: 'November', total: '1203.45'},
+    {month: 'December', total: '2103.54'}
+  ];
+  totalExpense = 'No Month Selected';
+  total: {month: string, total: string};
 
   expenses: Expense[] = [];
   _subscription: Subscription;
@@ -36,5 +53,14 @@ export class ExpensesComponent implements OnInit {
 
   modifyVisibility(): void {
     this.formVisible = false;
+  }
+
+  getTotal(value): void {
+    if (value !== undefined) {
+      this.total =  this.tempTotals.find(total => total.month === value);
+      this.totalExpense = '$' + this.total.total;
+    } else {
+      this.totalExpense = '$0.00';
+    }
   }
 }
